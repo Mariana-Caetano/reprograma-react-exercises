1259c39991c7e9cb1a3064e524983e26cf7129e9
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2022-06-30 22:47:48 -0300

    added Image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Cards from './Cards'
+import Image from './Image/Image'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
       <>
+        <Image />
         <Cards />
       </>
         
---
+++ src/Image/Image.jsx
@@ -0,0 +1,7 @@
+function Image() {
+    return(
+        <img src="https://i.pinimg.com/originals/cb/5e/8c/cb5e8cd212ea2c85e91b891b4bbfbc71.jpg" alt="Cansada" />
+    )
+}
+
+export default Image
